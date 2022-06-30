8e734fe3b2c8bd2fea7d4c402c41226e0db012a6
Rutesh Shah <96240108+ruteshdak@users.noreply.github.com>
2022-06-30 04:55:04 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  float salary,hra,da,gross_salary;
+  printf("Enter your salary: \n");
+  scanf("%f",& salary);
+  da = 0.4*salary;
+  hra = 0.2*salary;
+  gross_salary = da+hra+salary;
+  printf("gross salary= %f",gross_salary);
+  return 0;
+}
