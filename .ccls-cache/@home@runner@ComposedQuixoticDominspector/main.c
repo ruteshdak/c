073b5402690c994af627b327de15aa52259029c5
Rutesh Shah <96240108+ruteshdak@users.noreply.github.com>
2022-07-03 12:02:04 +0000

    reverse number

--- .ccls-cache/@home@runner@ComposedQuixoticDominspector/main.c
+++ .ccls-cache/@home@runner@ComposedQuixoticDominspector/main.c
@@ -1,35 +1,26 @@
-#include <stdio.h>
-int main(){
-  int year,basic_year=1900, remaining_year,leap_year,total_days,days;
-  printf("Enter the year");
-  scanf("%d",& year);
-  year = year-1-(basic_year);
-  leap_year = year/4;
-  remaining_year = year-leap_year;
-  total_days = (remaining_year*365) + (leap_year*366)+1;
-  days = total_days%7;
-  if(days==0){
-    printf("monday");
-  }
-  else if(days==1){
-    printf("Tuesday");
-  }
-  else if(days == 2){
-    printf("Wednesday");
-  }
-  else if(days ==3){
-    printf("Thursday");
-  }
-  else if(days ==4){
-    printf("Friday");
-  }
-  else if(days ==5){
-    printf("Saturday");
-  }
-  else if(days ==6){
-    printf("Sunday");
-  }
-  else {
-    printf("Wrong entry");
-  }
+
+#include<stdio.h>
+int main()
+{
+    int num, n, r_num=0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    n = num;
+    while(num!=0)
+    {
+        r_num = r_num * 10;
+
+        r_num = num % 10 + r_num;
+
+        num = num/10;
     }
+    printf("Reversed Number of %d is %d\n",n, r_num);
+
+    if (n==r_num)
+        printf("Input Number %d & Reversed Number %d are equal", n, r_num);
+    else
+        printf("Input Number %d & Reversed Number %d are not equal", n, r_num);
+
+   return 0;
+}
